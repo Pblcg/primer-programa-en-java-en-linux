18149e21789f737e97a794be3b37853d9a34c798
Pablo <pablocg7518@gmail.com>
2023-10-03 12:46:04 +0200

    Muestra mi lugar favorito

--- prueba.java
+++ prueba.java
@@ -3,6 +3,7 @@ public class prueba {
 	public static void main(String args[]){
 
 		System.out.println("Mi deporte favorito es la caza");
+		System.out.println("Mi lugar favorito es el medio natural de mi pueblo");
 
 	}
 
