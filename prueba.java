860ad70a0f10f54e6fce7d3e595d836be0a9d62d
Pablo <pablocg7518@gmail.com>
2023-10-03 12:48:31 +0200

    Muestra mi deporte favorito, mi lugar favorito y mi red social favorito

--- prueba.java
+++ prueba.java
@@ -4,6 +4,7 @@ public class prueba {
 
 		System.out.println("Mi deporte favorito es la caza");
 		System.out.println("Mi lugar favorito es el medio natural de mi pueblo");
+		System.out.println("Mi red social favorita es TikTok");
 
 	}
 
